54e5888d301776ba6ab764886eebc235e6bebe89
Eazel <eazeldonf1@yahoo.com>
2023-06-09 23:48:34 +0200

    Update  garment/setting.py

--- garment/settings.py
+++ garment/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # .env
-DEBUG = config('DEBUG', default=True, cast=bool)  # True
+#ORIGINAL DEBUG = config('DEBUG', default=True, cast=bool)  # True
+DEBUG = config('DEBUG', cast=bool)
 
 #FOR aws
 #ALLOWED_HOSTS = []
